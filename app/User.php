2adceec11d0033295028b24fc0464b5e4a218f3f
hek23 <hector.fuentealba@usach.cl>
2016-12-14 20:30:37 -0300

    full fix

--- app/User.php
+++ app/User.php
@@ -20,7 +20,7 @@ class User extends Authenticatable
     protected $fillable = [
         'ID_COMMUNE',
         'USERNAME',
-        'password',
+        'PASSWORD',
         'EMAIL',
         'PHONE',
         'FIRSTNAME',
