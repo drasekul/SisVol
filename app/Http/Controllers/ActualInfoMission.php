8f35a41fef1ec4f236899401f3c21d7b428d1f67
drasekul <dragomir.sekul@usach.cl>
2016-12-15 17:01:08 -0300

    se hizo el controlador de ActualInfoMission con return tipo dd() falta hacer la vista

--- app/Http/Controllers/ActualInfoMission.php
+++ app/Http/Controllers/ActualInfoMission.php
@@ -8,7 +8,11 @@ use Illuminate\Http\Request;
 class ActualInfoMission extends Controller
 {
     public function index(){
-      return view('EmergencyAttendant/actualInfoMission');
+      $mytime = \Carbon\Carbon::now()->format('Y-m-d');
+      //dd($mytime);
+      dd(DB::select('select M.NAME_MISION, M.CONDITION_MISION from MISION M inner join USER_ROLE UR on M.ID_USER_ROLE = UR.ID_USER_ROLE where UR.ID_USER_ROLE = '.session('userRol').' and M.INITIALDATE_MISION <= "'.$mytime.'" limit 1;'));
+
+      //return view('EmergencyAttendant/actualInfoMission');
     }
 
 }
