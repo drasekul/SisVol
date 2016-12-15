4cdfeae6947d3e30254014965c9a466476f70572
Tisks <leiser.mahu@gmail.com>
2016-12-15 18:31:41 -0300

    Update infoOnAbilities.blade.php

--- resources/views/Voluntary/infoOnAbilities.blade.php
+++ resources/views/Voluntary/infoOnAbilities.blade.php
@@ -4,13 +4,13 @@
       <div class="row">
         <div class="col-sm-3 col-md-2 sidebar">
           <ul class="nav nav-sidebar">
-            <li>
-			      <a href="/actualTask/index"><i class="fa fa-user fa-2x fa-fw"></i> Tarea Actual</a>
+            <li class="active"><a href="/infoOnAbilities/index">Gestion Habilidades <span class="sr-only">(current)</span></a></li>           <li>
+			      <li>
+            <a href="/actualTask/index"><i class="fa fa-user fa-2x fa-fw"></i> Tarea Actual</a>
             </li>
 			      <li>
             <a href="/taskRequests/index"><i class="fa fa-book fa-2x fa-fw"></i> Solicitudes Tareas</a>
             </li>
-            <li class="active"><a href="/infoOnAbilities/index">Gestion Habilidades <span class="sr-only">(current)</span></a></li>
 			      <li>
             <a href="/personalData/index"><i class="fa fa-book fa-2x fa-fw"></i>Datos Personales</a>
             </li>
