32b42fe351178adc77d0778d1f5f42166529e000
Tisks <leiser.mahu@gmail.com>
2016-12-15 18:33:15 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,20 +20,22 @@ Route::any('/login/index','Redirect@redir');
 Route::get('/test', 'TestController@index');
 Route::get('/home', 'HomeController@index');
 Route::any('/redirect','Redirect@register');
-Route::any('/Voluntario/index','Voluntario@index');
-Route::any('/Supervisor/index','Supervisor@index');
-Route::any('/Administrador/index','Administrador@index');
 Route::get('/actualInfoMission/index','ActualInfoMission@index');
 Route::get('/actualTask/index','ActualTask@index');
 Route::get('/addExtraVolunteers/index','AddExtraVolunteers@index');
 Route::get('/addMissions/index','AddMissions@index');
 Route::any('/defineEmergency/index','DefineEmergency@index');
-Route::post('/defineEmergency/regEmer','DefineEmergency@regEmer');
 Route::get('/emergencyManagement/index','EmergencyManagement@index');
 Route::get('/emergencyManagement/index','EmergencyManagement@index');
+
 Route::get('/infoOnAbilities/index','InfoOnAbilities@index');
 Route::get('/personalData/index','PersonalData@index');
 Route::get('/taskAssignment/index','TaskAssignment@index');
-Route::get('/taskRequests/index','TaskRequests@index');
+Route::get('/requestStatus/index','RequestStatus@index');
 Route::get('/taskStatus/index','TaskStatus@index');
+Route::get('/taskRequests/index','TaskRequests@index');
+
 Route::get('/voluntaryRequestsChoice/index','VoluntaryRequestsChoice@index');
+
+Route::post('/defineEmergency/regEmer','DefineEmergency@regEmer');
+
