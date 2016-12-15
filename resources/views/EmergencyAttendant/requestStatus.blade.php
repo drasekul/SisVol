2fc75efb5a50761f955e0085667e10ed694d08eb
Tisks <leiser.mahu@gmail.com>
2016-12-15 18:29:46 -0300

    Update requestStatus.blade.php

--- resources/views/EmergencyAttendant/requestStatus.blade.php
+++ resources/views/EmergencyAttendant/requestStatus.blade.php
@@ -1,3 +1,4 @@
+
 @extends('layouts.dashboard')
 @section('content')
 
@@ -6,24 +7,18 @@
         <div class="col-sm-3 col-md-2 sidebar">
           <ul class="nav nav-sidebar">
            <ul class="nav nav-sidebar">
-           <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
+            <li class="active"><a href="/requestStatus/index">Listado de Solicitudes <span class="sr-only">(current)</span></a></li>       
             <li>
-			<a href="datos_personales.html"><i class="fa fa-user fa-2x fa-fw"></i> Datos Personales</a>
-             </li>
-			<li>
-             <a href="datos_personales.html"><i class="fa fa-book fa-2x fa-fw"></i> Información Misión Actual</a>
+            <a href="/taskAssignment/index"><i class="fa fa-book fa-2x fa-fw"></i>Gestion Agregar Tareas</a>                          
             </li>
             <li>
-            <a href="gestionar_reclamos.html"><i class="fa fa-book fa-2x fa-fw"></i>Gestion Agregar Tareas</a>                          
+            <a href="/voluntaryRequestsChoice/index"><i class="fa fa-user fa-2x fa-fw"></i>Solicitud de Voluntarios</a>                          
             </li>
-			<li>
-            <a href="gestionar_reclamos.html"><i class="fa fa-user fa-2x fa-fw"></i>Solicitud de Voluntarios</a>                          
+            <li>
+            <a href="/actualInfoMission/index"><i class="fa fa-user fa-2x fa-fw"></i>Información Misión Actual</a>                          
             </li>
-			<li>
-            <a href="gestionar_reclamos.html"><i class="fa fa-user fa-2x fa-fw"></i>Estado de Solicitudes</a>                          
-            </li>
-			<li>
-            <a href="gestionar_reclamos.html"><i class="fa fa-book fa-2x fa-fw"></i>Estado de Tareas</a>                          
+            <li>
+            <a href="/taskStatus/index"><i class="fa fa-book fa-2x fa-fw"></i>Estado de Tareas</a>                          
             </li>
           </ul>
             <div style="text-align:center;height:30px;margin: 10px">
@@ -36,7 +31,7 @@
         <div class="col-sm-offset-3 col-sm-9 col-md-10 col-md-offset-2 main">          
           <div class="panel panel-default">
                         <div class="panel-heading">
-                            Datos Personales
+                            Listado de Solicitudes
                         </div>
                         <!-- /.panel-heading -->
                         <div class="panel-body">
@@ -44,14 +39,14 @@
                                 <table class="table">
                                     <tr>
                                           <th style="width:10%"><b>Nombre de tarea:</b></th>
-										   <th style="width:40%"><select class="form-control" name="ddlNombreTarea">
+                       <th style="width:40%"><select class="form-control" name="ddlNombreTarea">
                                                 <option value="nombreTarea1">Tarea1</option>
                                                 </select>
                                             </th>
-											
-											
-									</tr>	
-									<table class="table table-bordered">
+                      
+                      
+                  </tr> 
+                  <table class="table table-bordered">
                                     <tr>
                                         <th style="width:20%">Estado Solicitud</th>
                                         <th style="width:20%">Nombre</th>
@@ -59,48 +54,48 @@
                                         <th style="width:20%">Maestria</th>
                                         <th style="width:20%">Ubicación actual</th>
                                     </tr>
-										<td>	
-										   <div class="col-md-20">											
-											   <input class="form-control" type="text" readonly="true">
-										   </div>										
-										</td>
+                    <td>  
+                       <div class="col-md-20">                      
+                         <input class="form-control" type="text" readonly="true">
+                       </div>                   
+                    </td>
                                             
-										
-										<td>	
-										   <div class="col-md-20">											
-											   <input class="form-control" type="text" readonly="true">
-										   </div>										
-										</td>
-										
-                                       <td>										
-										 <select multiple class="form-control" id="areasDesempeño">
-												<option>1</option>
-												<option>2</option>
-												<option>3</option>
-												<option>4</option>
-												<option>5</option>
-										</select>
-										</td>	
-										
-										<td>										
-										 <select multiple class="form-control" id="areasDesempeño">
-												<option>1</option>
-												<option>2</option>
-												<option>3</option>
-												<option>4</option>
-												<option>5</option>
-										</select>
-										</td>	
-										
+                    
+                    <td>  
+                       <div class="col-md-20">                      
+                         <input class="form-control" type="text" readonly="true">
+                       </div>                   
+                    </td>
+                    
+                                       <td>                   
+                     <select multiple class="form-control" id="areasDesempeño">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                    </select>
+                    </td> 
+                    
+                    <td>                    
+                     <select multiple class="form-control" id="areasDesempeño">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                    </select>
+                    </td> 
+                    
                                         <td>
-										   <div class="col-md-20">											
-											 <input class="form-control" type="text" readonly="true">
-										   </div>
-										</td>
+                       <div class="col-md-20">                      
+                       <input class="form-control" type="text" readonly="true">
+                       </div>
+                    </td>
                                      </table>
                                 </table>
                             </div>
-							<div class="col-sm-offset-5 main"> <INPUT button type="button" value="Enviar" class="btn btn-primary btn-lg"></button></div>
+              <div class="col-sm-offset-5 main"> <INPUT button type="button" value="Enviar" class="btn btn-primary btn-lg"></button></div>
                             <!-- /.table-responsive -->
                         </div>
                         <!-- /.panel-body -->
