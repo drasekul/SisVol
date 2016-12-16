1f7e995a45a13042b8edbbb84a7150e84d2bd5a7
hek23 <hector.fuentealba@usach.cl>
2016-12-16 00:59:16 -0300

    Se crea el controlador para listar voluntarios de una mision

--- app/Http/Controllers/MisionController.php
+++ app/Http/Controllers/MisionController.php
@@ -7,9 +7,40 @@ use Illuminate\Http\Request;
 
 class MisionController extends Controller{
   public function mostrarVoluntarios(){
-    //Se consultan los voluntarios asignados
-    //$voluntarios_db = DB::select("CALL `mision_volunteers` ()");
-    dd($voluntarios_db);
+          //$nombre_mision = $_POST[mision];
+          $nombre_mision = "ornare lectus justo";
+          $voluntarios_db = DB::select("CALL `mision_volunteers` (?)",[$nombre_mision]);
+          $voluntarios=collect();
+          foreach ($voluntarios_db as $voluntario_db){
+            $voluntario=[];
+            //Se agrega el nombre del voluntario
+            $voluntario = array_add($voluntario, 'nombreVoluntario', $voluntario_db->FIRSTNAME);
+            //Se agrega el apellido del voluntario
+            $voluntario = array_add($voluntario, 'apellidoVoluntario', $voluntario_db->LASTNAME);
+            //Se agrega el telefono del voluntario
+            $voluntario = array_add($voluntario, 'fonoVoluntario', $voluntario_db->RUT);
+            //Se agrega el mail del voluntario
+            $voluntario = array_add($voluntario, 'mailVoluntario', $voluntario_db->RUT);
+            //Se agregan las referencias geograficas de origen del voluntario
+            $comuna = array_first(DB::select('select C.* from COMMUNE C WHERE C.ID_COMMUNE=(?)',[$voluntario_db->ID_COMMUNE]));
+            $voluntario = array_add($voluntario, 'comunaVoluntario', $comuna->NAME_COMMUNE);
+            $provincia = array_first(DB::select('select P.* from PROVINCE P WHERE P.ID_PROVINCE=(?)',[$comuna->ID_PROVINCE]));
+            $voluntario = array_add($voluntario, 'provinciaVoluntario', $provincia->NAME_PROVINCE);
+            $region = array_first(DB::select('select R.NAME_REGION from REGION R WHERE R.ID_REGION=(?)',[$provincia->ID_REGION]));
+            $voluntario = array_add($voluntario, 'regionVoluntario', $region->NAME_REGION);
+            //Se agrega el promedio de puntuación
+            $scoreVoluntario = array_first(DB::select('select AVG(S.POINTS) AS p from SCORE S where S.USE_ID_USER = (?)', [$voluntario_db->ID_USER]));
+            //Se agrega el score promedio
+            if (is_null($scoreVoluntario->p)){
+              $voluntario = array_add($voluntario, 'scoreVoluntario', 0);
+            }
+            if (!(is_null($scoreVoluntario->p)))  {
+              $voluntario = array_add($voluntario, 'scoreVoluntario', $scoreVoluntario->p);
+            }
+
+            //Se agrega el voluntario a la lista
+            $voluntarios->push($voluntario);
+          }
   }
   public function actualInfoMission(){
     $mytime = \Carbon\Carbon::now()->format('Y-m-d');
--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,10 +7,10 @@ use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-  public function index(){
-    $nombre_mision = "ornare lectus justo";
-    $voluntarios_db = DB::select("CALL `mision_volunteers` (?)",[$nombre_mision]);
-    dd($voluntarios_db);
+  public function mostrarVoluntarios(){
+
+
+    dd($voluntarios);
     return view('test')->with('comunas', $test);
   }
 }
